f4806b68d943a231c973000899631ad635398d19
Mertcan <101744404+mertcantopcu01@users.noreply.github.com>
2023-04-20 21:12:18 +0300

    DENEME
    DENEME AÇIKLAMA

--- app/src/main/java/com/mert/malkinfo/profilo/mainler/GirisActivity.kt
+++ app/src/main/java/com/mert/malkinfo/profilo/mainler/GirisActivity.kt
@@ -19,6 +19,9 @@ class GirisActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGirisBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        /*
+        TEST
+         */
 
         firebaseAuth = FirebaseAuth.getInstance()
 
